7c253a3ba87b1b35a28ceec43d5671199b759c87
Chronoss0518 <sinyusuke5000@gmail.com>
2022-01-03 20:53:23 +0900

    Battle関係のLifePointを追加

---
+++ Editor/BattleType/LifePointEditor.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+
+[CustomEditor(typeof(LifePoint))]
+public class LifePointEditor : CustomInspectorBase
+{
+
+    public override void OnInspectorGUI()
+    {
+
+        base.OnInspectorGUI();
+
+        var obj = target as LifePoint;
+
+        {
+
+            int tmp = obj.maxLifePoint;
+            Slider(ref tmp, obj.lowMaxLifePoint, obj.highMaxLifePoint, "Max Life Point");
+            obj.maxLifePoint = tmp;
+        }
+
+        {
+
+            int low = obj.lowMaxLifePoint, high = obj.highMaxLifePoint;
+            Label("Set Max Life Point Range");
+            BeginHorizontal();
+            InputField(ref low, "Low:");
+            EndHorizontal();
+
+            BeginHorizontal();
+            InputField(ref high, "High:");
+            EndHorizontal();
+
+            obj.lowMaxLifePoint = low;
+            obj.highMaxLifePoint = high;
+        }
+
+    }
+}
---
+++ Script/BattleType/LifePoint.cs
@@ -0,0 +1,46 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class LifePoint : MonoBehaviour
+{
+    protected int lp = 0;
+
+    protected int maxLP = 0;
+
+    protected int lMaxLP = 0,hMaxLP = 100;
+
+    public int maxLifePoint { set { maxLP = value; } get { return maxLP; } }
+    public int lowMaxLifePoint { set { lMaxLP = value < hMaxLP ? value : hMaxLP - 1; } get { return lMaxLP; } }
+    public int highMaxLifePoint { set { hMaxLP = value > lMaxLP ? value : lMaxLP + 1; } get { return hMaxLP; } }
+
+    public int lifePoint { set { lp = value; } get { return lp; } }
+
+    public void Heal(int _heal)
+    {
+        lp += _heal;
+        lp = lp > maxLP ? maxLP : lp;
+    }
+
+    public void Damage(int _damage)
+    {
+        lp -= _damage;
+        lp = lp < 0 ? 0: lp;
+    }
+    public bool IsDeath()
+    {
+        return (lp <= 0);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
