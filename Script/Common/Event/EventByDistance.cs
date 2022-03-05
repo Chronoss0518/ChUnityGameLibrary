0fdf2d784625fbda0803a98d6d792682b41147a8
Chronoss0518 <sinyusuke5000@gmail.com>
2022-03-05 23:59:51 +0900

    距離によってイベントを発生させる機能を追加

---
+++ Script/Common/Event/EventByDistance.cs
@@ -0,0 +1,43 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+
+public class EventByDistance : MonoBehaviour
+{
+
+    public float distance = 0.0f;
+
+    public GameObject targetObject = null;
+
+    public UnityEngine.Events.UnityEvent action = new UnityEngine.Events.UnityEvent();
+
+    bool playFlg = false;
+
+    public void ReSet()
+    {
+        playFlg = false;
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (playFlg) return;
+
+        Vector3 tmp = (targetObject != null ? targetObject.transform.position : Vector3.zero);
+
+        Vector3 pos = tmp - gameObject.transform.position;
+
+        if(pos.magnitude <= distance)return;
+
+        action.Invoke();
+
+        playFlg = true;
+    }
+}
