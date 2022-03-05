2d09a58c7c9fa2740524a70e48afa413de57a561
Chronoss0518 <sinyusuke5000@gmail.com>
2022-03-06 00:01:10 +0900

    オブジェクトをサイズを操作する機能を追加

---
+++ Script/Common/TransformController/ObjectScale.cs
@@ -0,0 +1,94 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectScale : MonoBehaviour
+{
+    public float scaleSize = 0.0f;
+
+    public GameObject moveTarget = null;
+
+    private Vector3 scale = Vector3.zero;
+
+    protected void ZeroTest()
+    { 
+        var tmpScale = transform.localScale;
+
+        tmpScale.x  = transform.localScale.x > 0.0f ? transform.localScale.x : 0.0f;
+        tmpScale.y  = transform.localScale.y > 0.0f ? transform.localScale.y : 0.0f;
+        tmpScale.z  = transform.localScale.z > 0.0f ? transform.localScale.z : 0.0f;
+    }
+
+    public void AddMoveSize(float _mSize) { scaleSize += _mSize; }
+
+    public void SubMoveSize(float _mSize) { scaleSize -= _mSize; }
+
+    public void MulMoveSize(float _mSize) { scaleSize *= _mSize; }
+
+    public void DivMoveSize(float _mSize) { scaleSize /= _mSize != 0.0f ? _mSize : 1.0f; }
+
+    public void SetMoveSize(float _mSize) { scaleSize = _mSize; }
+
+    public float GetMoveSize() { return scaleSize; }
+
+    public void MoveFoward()
+    {
+        scale += Vector3.forward;
+    }
+
+    public void MoveBack()
+    {
+        scale += Vector3.back;
+    }
+
+    public void MoveRight()
+    {
+        scale += Vector3.right;
+    }
+
+    public void MoveLeft()
+    {
+        scale += Vector3.left;
+    }
+    public void MoveUp()
+    {
+        scale += Vector3.up;
+    }
+
+    public void MoveDown()
+    {
+        scale += Vector3.down;
+    }
+
+    public void MoveFloatSize(float _mSize)
+    {
+        scale += new Vector3(_mSize, _mSize, _mSize);
+    }
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        if (moveTarget != null) return;
+        moveTarget = gameObject;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Start();
+
+        if(moveTarget != null)
+        {
+            Rigidbody body = moveTarget.GetComponent<Rigidbody>();
+
+            if (body != null) body.transform.localScale = scale;
+            else moveTarget.transform.localScale = scale;
+
+        }
+        
+        ZeroTest();
+
+        scale = Vector3.zero;
+
+    }
+}
