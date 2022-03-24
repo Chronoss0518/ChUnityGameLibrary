1399d0f3071ae8e6ba99bb9ee5349f95027d0e42
Chronoss0518 <sinyusuke5000@gmail.com>
2022-03-24 19:41:37 +0900

    ExistGameObjectEventを更新

--- Script/Common/Event/ExistGameObjectEvent.cs
+++ Script/Common/Event/ExistGameObjectEvent.cs
@@ -2,19 +2,13 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class ExistGameObjectEvent : MonoBehaviour
+public class ExistGameObjectEvent : GameObjectTargetSelector
 {
-    public GameObject targetObject = null;
     
     public UnityEngine.Events.UnityEvent trueEvent = new UnityEngine.Events.UnityEvent();
     public UnityEngine.Events.UnityEvent falseEvent = new UnityEngine.Events.UnityEvent();
 
-    public void SetGameObject(GameObject _obj)
-    {
-        targetObject = _obj;
-    }
-
-    public void RunExistEvent()
+    public void RunEvent()
     {
         if(targetObject != null)
         {
